da43a18e808f710ec931cc630112e6790b96266c
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-08-01 17:56:11 +0100

    Add solution to problem 02

--- main.ml
+++ main.ml
@@ -5,3 +5,15 @@ let rec last (lst : 'a list) : 'a option =
 let () =
   assert (last [ "a"; "b"; "c"; "d" ] = Some "d");
   assert (last [] = None)
+
+(* Problem 02 - Last Two Elements of a List *)
+let rec last_two (lst : 'a list) : ('a * 'a) option =
+  match lst with
+  | [ x; y ] -> Some (x, y)
+  | _ :: rest -> last_two rest
+  | _ -> None
+
+let () =
+  assert (last_two [ "a"; "b"; "c"; "d" ] = Some ("c", "d"));
+  assert (last_two [ "a" ] = None);
+  assert (last_two [] = None)
