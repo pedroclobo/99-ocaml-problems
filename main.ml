8a07a4fdf5182bc1e3c0cc4f5d9d21a6b948e2b8
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-08-01 18:07:50 +0100

    Add solution to problem 04

--- main.ml
+++ main.ml
@@ -30,3 +30,14 @@ let () =
   assert (nth [ 1; 2; 3 ] 3 = Some 3);
   assert (nth [ "a"; "b"; "c"; "d" ] 0 = None);
   assert (nth [] 1 = None)
+
+(* Problem 04 - Length of a List *)
+let length (lst : 'a list) : int =
+  let rec length_aux (lst : 'a list) (n : int) =
+    match lst with [] -> n | _ :: rest -> length_aux rest (n + 1)
+  in
+  length_aux lst 0
+
+let () =
+  assert (length [ "a"; "b"; "c" ] = 3);
+  assert (length [] = 0)
